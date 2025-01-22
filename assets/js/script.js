b9fe9a2ea5ef15d66b278788d62b67c28b3bf51c
agweb12 <graziani1112@gmail.com>
2025-01-22 10:04:28 +0100

    version 1.5.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-var statisticsClient = document.querySelector('.clients-statistics');
-var quoteClient = document.querySelector('.quote-client');
+let statisticsClient = document.querySelector('.clients-statistics');
+let quoteClient = document.querySelector('.quote-client');
 
 //si on clique sur la classe .clients-statistics, on ajoute une classe .active à la classe .quote-client sauf si elle a déjà la classe .active
 statisticsClient.addEventListener('click', function () {
@@ -7,5 +7,26 @@ statisticsClient.addEventListener('click', function () {
         quoteClient.classList.add('visible');
     } else {
         quoteClient.classList.remove('visible');
+    }
+});
+
+let iconMenu = document.querySelector('.menu-aside');
+let menuAside = document.querySelector('.aside');
+let asideBox = document.querySelector('.aside-box');
+
+//si on clique sur la classe .menu-aside, on ajoute une classe .active à la classe .aside sauf si elle a déjà la classe .active
+iconMenu.addEventListener('click', function () {
+    if (!menuAside.classList.contains('active')) {
+
+        // menuAside.style.display = 'flex';
+        menuAside.classList.add('active');
+        menuAside.style.right = '0px';
+        iconMenu.style.rotate = '90deg';
+
+    } else {
+
+        menuAside.classList.remove('active');
+        iconMenu.style.rotate = '0deg';
+        menuAside.style.right = '-200px';
     }
 });
